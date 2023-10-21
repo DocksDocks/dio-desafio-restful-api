5da9dd27a5bd915fef85cbc32bf9f7f4aff2e8f4
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2023-10-21 17:57:31 -0300

    Initial commit

---
+++ src/main/java/docks/api/santanderdevweek2023/Application.java
@@ -0,0 +1,13 @@
+package docks.api.santanderdevweek2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
